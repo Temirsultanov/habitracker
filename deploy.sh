09b4d80a1ddff78687866e2ed74f405c87e59554
Мухаммад <human.twm@gmail.com>
2022-09-07 01:11:19 +0300

    Delete empty string from deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,9 @@
-#!/usr/bin/env sh
-
 set -e
-
 npm run build
-
 cd dist
-
 git init
 git checkout -b main
 git add -A
 git commit -m 'deploy'
-
 git push -f git@github.com:temirsultanov/habitracker.git main:gh-pages
-
 cd -
